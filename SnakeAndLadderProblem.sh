506e8cd9df8376c481bfd11d1841a3fe2c2a711b
Jitesh-Dabir <jitudabir9@gmail.com>
2020-02-24 12:22:52 +0530

    ADDED:The player rolls the die to get number between 1 to 6

--- SnakeAndLadderProblem.sh
+++ SnakeAndLadderProblem.sh
@@ -5,3 +5,15 @@ echo ".........................................Welcome to Snake And Ladder......
 #CONSTANT
 NUMBER_OF_PLAYER=1
 STARTING_POSITION=0
+
+#FUNCTION TO GET DIE NUMBER BETWEEN 1 TO 6
+function rollDie()
+{
+randomDieNumber=$((RANDOM%6+1))
+echo $randomDieNumber
+}
+
+#FUNCTION CALL TO GET RANDOM DIE NUMBER BETWEEN 1 TO 6
+echo "Die number:$(rollDie)"
+
+
