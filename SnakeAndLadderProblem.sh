a2f2267f60e82b1ef6c915f37f37799ea7b30368
Jitesh-Dabir <jitudabir9@gmail.com>
2020-02-24 12:10:33 +0530

    ADDED:Snake and ladder game played with single player at initialize the start position

--- SnakeAndLadderProblem.sh
+++ SnakeAndLadderProblem.sh
@@ -1,3 +1,7 @@
 #!/bin/bash -x
 
 echo ".........................................Welcome to Snake And Ladder........................................"
+
+#CONSTANT
+NUMBER_OF_PLAYER=1
+STARTING_POSITION=0
