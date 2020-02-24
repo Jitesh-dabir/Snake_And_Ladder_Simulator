29d822a588d565db3a2bfbd5a6160bbb12f6b74e
Jitesh-Dabir <jitudabir9@gmail.com>
2020-02-24 13:00:03 +0530

    ADDED:Repeat till the player reaches the winning position 100 and if position goes in neagetive then start with zero

--- SnakeAndLadderProblem.sh
+++ SnakeAndLadderProblem.sh
@@ -5,7 +5,11 @@ echo ".........................................Welcome to Snake And Ladder......
 #CONSTANT
 NUMBER_OF_PLAYER=1
 STARTING_POSITION=0
+FLAG=0
+SAFE_POSITION=100
 
+#VARIABLE
+Position=0
 
 #FUNCTION TO GET DIE NUMBER BETWEEN 1 TO 6
 function rollDie()
@@ -15,39 +19,49 @@ function rollDie()
 }
 
 #FUNCTION CALL TO GET RANDOM DIE NUMBER BETWEEN 1 TO 6
-echo "Random number between 1 to 6 is :$(rollDie)"
+rollDie
 
 #GENERATE RANDOM NUMBER FOR CHECKING THEY ARE NO PLAY,LADDER,SNAKE
 randomCheckNumber=$((RANDOM%3+1))
 
 #CHECK NO PLAY,SNAKE,LADDER
-case $randomCheckNumber in
-	1)
-		echo ".........................................."
-	  	echo ""
-		position=$(($position))
-		echo "No Play - position of user =$position" 
-		echo ""
-		echo ".........................................."	
-		;;
+while [[ $position -ne $SAFE_POSITION ]]
+do
+	randomNumber=$((RANDOM%3+1))
+	case $randomNumber in
 
-	2)
-		firstRandomNumber=$((RANDOM%6+1))
-		echo ".........................................."
-		echo ""
-		position=$(($position+$firstRandomNumber))
-		echo "Ladder - position of user =$position" 
-		echo ""
-		echo ".........................................."
-		;;
+		1)
+			echo ".........................................."
+	   	echo ""
+			position=$position
+			echo "No Play - position of user1 =$position"
+			echo ""
+		   echo ".........................................."
+			;;
 
-	3)
-		secondRandomNumber=$((RANDOM%6+1))
-		echo ".........................................."
-		echo ""
-		position=$(($position-$secondRandomNumber))
-		echo "Snake - position of user =$position" 
-		echo ""
-		echo ".........................................."
-		;;
-esac
+		2)
+			firstRandomNumber=$((RANDOM%6+1))
+			echo ".........................................."
+			echo ""
+			position=$(($position+$firstRandomNumber))
+			echo "Ladder - position of user =$position"
+			echo ""
+			echo ".........................................."
+			;;
+
+		3)
+			secondRandomNumber=$((RANDOM%6+1))
+			echo ".........................................."
+			echo ""
+			position=$(($position-$secondRandomNumber))
+			if [ $position -lt $STARTING_POSITION ]
+			then
+				position=$STARTING_POSITION
+			fi
+			echo "Snake - position of user =$position"
+			echo ""
+			echo ".........................................."
+			;;
+	esac
+done
+echo "user win position=$position"
